a0b31aa10616ff9981988b70a8f8794cd45cfa94
Akshayaprabha <167444918+Akshayaprabha17@users.noreply.github.com>
2024-05-30 21:11:11 +0530

    Create  python.py
    swaping two numbers in different way

---
+++ python.py
@@ -0,0 +1,23 @@
+#way one
+"""c=input("Enter value of a=")
+d=input("Enter value of b=")
+print("a="+d)
+print("b="+c)"""
+#way two
+'''a=input("enter value of a=")
+b=input("enter value of b=")
+
+swape=a
+a=b
+b=swape
+print("a=",a)
+print("b=",b)'''
+#way three
+a=input("Enter the value of a=")
+b=input("enter the value of b=")
+#initial value
+'''print("the initial value of a="+a)
+print("the initial value of b="+b)'''
+#swape
+print("the swape value of a="+b)
+print("the swape value of b="+a)
